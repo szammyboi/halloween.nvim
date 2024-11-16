0ff018676b60bdf51a030ffe6773c7db2f5d76af
Sam Morse <78485053+szammyboi@users.noreply.github.com>
2024-11-15 22:12:49 -0500

    Update and rename darkrose.lua to halloween.lua

--- colors/darkrose.lua
+++
@@ -1,4 +0,0 @@
--- Setup theme with defaults if it is not already setup
-require("darkrose.config")
-
-require("darkrose").colorscheme()
---
+++ colors/halloween.lua
@@ -0,0 +1,4 @@
+-- Setup theme with defaults if it is not already setup
+require("halloween.config")
+
+require("halloween").colorscheme()
